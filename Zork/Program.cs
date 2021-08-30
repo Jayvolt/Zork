47920af313448d0d93210334e847704d741e6108
Jayvolt <jay1552@knights.ucf.edu>
2021-08-30 15:22:03 -0400

    Zork 1.2 InProgress

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,20 +8,53 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine().Trim().ToUpper();
+            string inputString = Console.ReadLine();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
 
-            if(inputString == "QUIT")
+          //  if(inputString == "QUIT")
+          //  {
+          //      Console.WriteLine("Thank you for playing");
+          //  }
+          //  else if(inputString == "LOOK")
+          //  {
+          //      Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.");
+          //  }
+          //  else
+          //  {
+          //      Console.WriteLine("Unrecognized command.");
+          //  }
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            Commands command;
+
+            switch(commandString)
             {
-                Console.WriteLine("Thank you for playing");
+                case "QUIT":
+                    command = Commands.QUIT;
+                    break;
+                case "LOOK":
+                    command = Commands.LOOK;
+                    break;
+                case "NORTH":
+                    command = Commands.NORTH;
+                    break;
+                case "SOUTH":
+                    command = Commands.SOUTH;
+                    break;
+                case "EAST":
+                    command = Commands.EAST;
+                    break;
+                case "WEST":
+                    command = Commands.WEST;
+                    break;
+                case "UNKNOWN":
+                    command = Commands.UNKNOWN;
+                    break;
             }
-            else if(inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+
         }
     }
 }
