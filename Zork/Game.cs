75f453d248c067cb35cbb5b5178d75871115b64c
Jayvolt <70279845+Jayvolt@users.noreply.github.com>
2021-10-01 13:17:26 -0400

    Zork 4.1 and Zork 4.2 in Class

---
+++ Zork/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Zork
+{
+    class Game
+    {
+        public World World { get; }
+
+        public static Game Load(string filename)
+        {
+            Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(filename));
+            return game;
+        }
+    }
+}
--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Zork
 {
@@ -15,7 +17,7 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            InitializeRoomDescription();
+            InitializeRooms("Rooms.json");
 
             Room previousRoom = null;
             Commands command = Commands.UNKNOWN;
@@ -58,33 +60,11 @@ namespace Zork
 
         private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
 
-        private static readonly Room[,] Rooms = {
-            { new Room("Rocky Trail"),new Room("South of House"),new Room("Canyon View") },
-            { new Room("Forest"),new Room("West of House"),new Room("Behind House")},
-            { new Room("Dense Woods"),new Room("North of House"),new Room("Clearing")}
-        };
+        private static Room[,] Rooms;
 
-        private static void InitializeRoomDescription()
+        private static void InitializeRooms(string roomsFilename)
         {
-            var roomMap = new Dictionary<string, Room>();
-            foreach (Room room in Rooms)
-            {
-                roomMap[room.Name] = room;
-            }
-
-
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
-
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions.";
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
-
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the winsows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
-
+            Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
         }
 
         private static readonly List<Commands> Directions = new List<Commands>
@@ -107,11 +87,11 @@ namespace Zork
             switch (command)
             {
                 case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
-                    Location.Row--;
+                    Location.Row++;
                     isValidMove = true;
                     break;
                 case Commands.SOUTH when Location.Row > 0:
-                    Location.Row++;
+                    Location.Row--;
                     isValidMove = true;
                     break;
 
